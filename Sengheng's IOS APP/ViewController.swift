11ab5c8028a85959386f0aed47c04eef771ed098
Sengheng <hongsengheng@LearniOSFreecoms-Mac.local>
2017-08-10 05:38:31 +0100

    Initial Commit

---
+++ Sengheng's IOS APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sengheng's IOS APP
+//
+//  Created by Sengheng on 10/08/2017.
+//  Copyright © 2017 Sengheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
